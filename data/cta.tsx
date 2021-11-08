055ed4337d86824f81f8d9a0911765880aff9b67
itsjuanmatus <juanmatusg16@gmail.com>
2021-11-08 15:38:55 -0600

    big bucket

---
+++ data/cta.tsx
@@ -0,0 +1,10 @@
+export const cta = [{
+    title: 'Big Bucket of Science',
+    subtitle: 'Ages 6+',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dignissimos iste, possimus sit totam ipsam a fugit voluptates laboriosam quam dolorum harum unde aspernatur excepturi facilis hic quod! Cumque, sequi.',
+    button: 'Shop now',
+    image: 'https://images.pexels.com/photos/5063442/pexels-photo-5063442.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    mobileOrder: 'first',
+    desktopOrder: 'last',
+
+}]
--- pages/image-cta.tsx
+++ pages/image-cta.tsx
@@ -1,30 +1,14 @@
 import React from 'react'
+import CTA from '../components/CTA'
+import { cta } from '../data/cta'
 
 
 export default function ImageCTA() {
     return (
         <div className="flex-col min-h-screen w-full bg-white py-10 px-10 m-auto">
-            <div className="grid md:grid-cols-2 place-content-center md:gap-x-16 gap-y-7 place-items-center">
-                <div
-                    className='flex justify-center p-6 bg-blue-200 border border-dashed border-black max-w-2xl md:order-last'
-                >
-                    <img
-                        className='object-cover'
-                        src="https://images.pexels.com/photos/5063442/pexels-photo-5063442.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
-                </div>
-                <div className=''>
-                    <h2 className="text-3xl md:text-4xl font-semibold text-blue-900 pr-16">
-                        Big Bucket of Science
-                    </h2>
-                    <h3 className='text-blue-300 text-xl md:text-2xl mt-2 kids-font'>Ages 6+</h3>
-                    <p className='mt-2 text-truegray-500 pr-16'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dignissimos iste, possimus sit totam ipsam a fugit voluptates laboriosam quam dolorum harum unde aspernatur excepturi facilis hic quod! Cumque, sequi.
-                    </p>
-                    <button className='text-blue-900 border-blue-900 p-3 border-2 font-semibold mt-6 md:px-6'>
-                        Shop now
-                    </button>
-                </div>
-            </div>
+            {cta && cta.map((e: any) => (
+                <CTA key={e.title} title={e.title} description={e.description} button={e.button} desktopOrder={e.desktopOrder} mobileOrder={e.mobileOrder} subtitle={e.subtitle} image={e.image} />
+            ))}
 
         </div>
     )
