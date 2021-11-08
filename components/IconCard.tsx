29563c117c1be8a896964b925f5e24d0a6f2afad
itsjuanmatus <juanmatusg16@gmail.com>
2021-11-08 15:25:27 -0600

    fixing width

--- components/IconCard.tsx
+++ components/IconCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function IconCard({title, description, icon}:any) {
     return (
         <div className="bg-white shadow-sm border border-gray-200 grid items-start rounded-xl p-6 gap-y-2">
-            <div className="text-truegray-700 w-full max-w-min">
+            <div className="text-truegray-700 w-full max-w-max">
                 {icon}
             </div>
             <h3 className='text-lg font-semibold text-truegray-700'>
