f175308d15f59661296cef92185df82be458ef42
itsjuanmatus <juanmatusg16@gmail.com>
2021-11-08 13:56:30 -0600

    fixed svg

--- pages/icon-cards.tsx
+++ pages/icon-cards.tsx
@@ -6,7 +6,6 @@ export default function IconCards() {
     return (
         <div className="flex-col min-h-screen w-full m-auto">
             <div className='grid md:grid-cols-3 gap-10 p-10'>
-
                 {data && data.map((e: any) => (
                     <IconCard title={e.title} description={e.description} icon={e.icon} key={`${e.title}`} />
                 ))}
