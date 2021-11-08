e83b16495fc67abda800caacc0e474c5161ec6ed
itsjuanmatus <juanmatusg16@gmail.com>
2021-11-08 14:14:50 -0600

    fixing svg

---
+++ pages/image-cta.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ImageCTA() {
+    return (
+        <div>
+            <img src="https://images.pexels.com/photos/5063442/pexels-photo-5063442.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+        </div>
+    )
+}
