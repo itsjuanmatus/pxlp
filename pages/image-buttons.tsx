90b78e4fc0677c427d1432923a88e5aaa0465967
itsjuanmatus <juanmatusg16@gmail.com>
2021-11-09 22:59:19 -0600

    image buttons

--- pages/image-buttons.tsx
+++ pages/image-buttons.tsx
@@ -4,15 +4,17 @@ import { ages } from '../data/ages'
 export default function ImageButtons() {
     return (
         <div className="min-h-screen bg-blue-100 w-full flex-col py-16">
-            <div className="grid place-items-center place-content-center gap-y-10">
+            <div className="grid place-items-center gap-y-10 w-full">
                 <img src="./planet.png" alt="logo" className="w-36 -my-16" />
-                <h2 className="text-blue-900 text-3xl text-center font-semibold px-16">Designed for every age and stage of development</h2>
-                <div className="grid grid-cols-2 md:inline-flex gap-x-5 gap-y-7">
+                <h2 className="text-blue-900 text-3xl text-center font-semibold px-16 lg:px-96">
+                    Designed for every age and stage of development
+                </h2>
+                <div className="grid grid-cols-2 md:flex flex-wrap w-full md:justify-between items-center gap-x-5 gap-y-7 px-10">
                     {ages && ages.map((e: any) => (
                         <div className="grid place-items-center" key={e.range}>
                             <img
                                 style={{
-                                    borderRadius: '5rem',
+                                    borderRadius: '7rem',
                                 }}
                                 className="w-36 h-36 object-cover  cursor-pointer hover:border-truegray-500 border-dashed border-4 p-1"
                                 src={e.image}
